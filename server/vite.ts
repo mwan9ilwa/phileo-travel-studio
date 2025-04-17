835ed1e94b59c6f3c904f48a07d2bd05f0936451
Mwangilwa Zimba <mwangilwa23@gmail.com>
2025-04-17 20:44:38 +0200

    config vercel

--- server/vite.ts
+++ server/vite.ts
@@ -36,7 +36,13 @@ export async function setupVite(app: Express, server: Server) {
         process.exit(1);
       },
     },
-    server: serverOptions,
+    server: {
+      middlewareMode: true,
+      hmr: {
+        server,
+      },
+      allowedHosts: true, // Allow all hosts
+    },
     appType: "custom",
   });
 
